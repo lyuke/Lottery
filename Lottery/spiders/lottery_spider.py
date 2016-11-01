7aad7a2757028edb9a20e46e2c7d07e3696eddbb
Lyuke <lk11nju@gmail.com>
2016-11-01 23:02:43 +0800

    udpate spider

--- Lottery/spiders/lottery_spider.py
+++ Lottery/spiders/lottery_spider.py
@@ -1,12 +1,15 @@
 import scrapy
+import lotteryItem from items
 
 class LotteryScrapy(scrapy.Spider):
     name = "lottery"
     allowed_domains = ["dmoz_orgs"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.lottery.gov.cn/historykj/history.jspx?_ltype=dlt"
     ]
 
     def parse(self, response):
-        print response.body
+        for sel in response.xpath('//tr[1]'):
+            item = lotteryItem()
+            item['issue'] = sel.xpath('//td[1]')
+            yield item
